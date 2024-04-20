75654f623ec873086c5dc980df1947ca8912e14a
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-04-20 14:42:59 +0300

    include router wallets in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import users, tours
+from app.routers import users, tours, wallets
 
 app = FastAPI()
 
 
 app.include_router(users.router)
 app.include_router(tours.router)
+app.include_router(wallets.router)
